e5ea7161fd0640284694991c63cbd2a28f9e47f0
Patryk Palej <patrykpalej@gmail.com>
2024-04-19 16:07:38 +0200

    Fire modification

--- cli3.py
+++ cli3.py
@@ -1,13 +1,7 @@
 import fire
 
 
-# def add(x, y):
-#     return x + y
-#
-# def subtract(x, y):
-#     return x - y
-
-class Calculator:
+class SimpleCalculator:
     def add(self, x, y):
         return x + y
 
@@ -20,6 +14,24 @@ class Calculator:
     def divide(self, x, y):
         return x/y
 
+
+class AdvancedCalculator:
+    def sqrt(self, x):
+        return x**0.5
+
+    def exp(self, x):
+        return 2.71 ** x
+
+
+class Pipeline:
+    def __init__(self):
+        self.simple = SimpleCalculator()
+        self.advanced = AdvancedCalculator()
+
+    def info(self):
+        return "Simple and advanced calculator"
+
+
 if __name__ == "__main__":
-    fire.Fire(Calculator)
+    fire.Fire(Pipeline)
     # fire.Fire({"add": add, "subtract": subtract})
